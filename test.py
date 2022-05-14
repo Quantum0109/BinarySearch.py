23d528a1f4b5624d7a3634dd13dba92bb2552831
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2022-05-14 13:24:20 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,23 +1,13 @@
-import random
-nums = [i for i in range(10000001)]
-goal = random.randint(0, 10000001)
-print(goal)
+import random 
+
+data = [i for i in range(101)]
+data.reverse()
+
 counter = 0
+print(data)
 
-first_idx = 0
-last_idx = len(nums) - 1
-mid_idx = (first_idx + last_idx) // 2
-
-while first_idx <= last_idx:
+while data != sorted(data):
+    random.shuffle(data)
     counter += 1
-    if goal == nums[mid_idx]:
-        print(f"число {goal} нашлось на индексе {mid_idx} за {counter} попыток")
-        break
-    elif goal > nums[mid_idx]:
-        first_idx = mid_idx + 1
-        mid_idx = (first_idx + last_idx) // 2
-    elif goal < nums[mid_idx]:
-        last_idx = mid_idx - 1
-        mid_idx = (first_idx + last_idx) // 2
-    else:
-        print(f"{goal} is not found")
+    print(f"список теперь: {data} за {counter} итераций")
+    
